fa21dc0a9b898f449130131e68957782d830e766
David Lains <dlains@gmail.com>
2015-10-09 15:23:29 -0700

    CA-3852 Fix method naming errors.
    Some method names coming from Zoho have numbers as the first character. This causes a problem when attempting to create a method. Added a method to convers digits to words.

--- lib/api_utils.rb
+++ lib/api_utils.rb
@@ -11,8 +11,36 @@ module ApiUtils
   end
 
   def self.string_to_symbol(s)
+    s = convert_digit_to_word(s) if s.match /^[0-9]/
     s.gsub!(/[()%]*/, '')
     s.gsub(' ', '_').downcase.to_sym
+  end
+
+  def self.convert_digit_to_word(s)
+    digit = s.match /^[0-9]/
+    case digit[0]
+    when '0'
+      s.gsub!('0','zero')
+    when '1'
+      s.gsub!('1','one')
+    when '2'
+      s.gsub!('2','two')
+    when '3'
+      s.gsub!('3','three')
+    when '4'
+      s.gsub!('4','four')
+    when '5'
+      s.gsub!('5','five')
+    when '6'
+      s.gsub!('6','six')
+    when '7'
+      s.gsub!('7','seven')
+    when '8'
+      s.gsub!('8','eight')
+    when '9'
+      s.gsub!('9','nine')
+    end
+    return s
   end
 
   def self.symbol_to_string(sym)
